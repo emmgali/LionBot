110254741a7e4ea610bb7f765b6be12f5a985bb2
Trekkar <noggaxiii@gmail.com>
2019-01-13 11:32:28 -0300

    procfile configured to WEB service. Now dynos should sleep when not used

--- index.js
+++ index.js
@@ -1,6 +1,14 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
 
+var express = require('express');
+var app = express();
+const PORT = process.env.PORT || 5000
+
+app.listen(PORT, function(){
+    console.log('App escuchando!');
+})
+
 const { Pool } = require('pg');
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
