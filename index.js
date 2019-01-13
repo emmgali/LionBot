10fa5f7ce74fac5479aca153d710d810cf8605d7
Trekkar <noggaxiii@gmail.com>
2019-01-13 11:15:14 -0300

    web proc function and saved env variables

--- index.js
+++ index.js
@@ -1,6 +1,13 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
 
+const { Pool } = require('pg');
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+  ssl: true
+});
+
+
 client.on('ready', () => {
     client.user.setActivity('bot en heroku', {type: 'WATCHING'});
     console.log('Up and loaded!');
@@ -25,5 +32,5 @@ client.on('message', message => {
     }
 });
 
-//Carefull! Private token!
-client.login("NTMzNjk3OTk4MDgyMDE1MjQy.Dxu3nw.Ylck6jw6ILKIY2UCisoaTfgWYR4");
+//Private token is saved on env variables. On local, is found on .env file. Also uploaded to heroku cloud.
+client.login(process.env.TOKEN);
